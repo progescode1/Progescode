deadd7075648d72d71187f85ef177e2d11f9a70e
nadia <progescode@gmail.com>
2024-05-03 12:02:15 +0200

    as

--- src/app/demo/components/auth/login/login.component.ts
+++ src/app/demo/components/auth/login/login.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
 
 @Component({
+    selector: "app-login",
     templateUrl: './login.component.html',
+    styleUrl :"./login.component.scss"
 })
 export class LoginComponent {
     rememberMe: boolean = false;
